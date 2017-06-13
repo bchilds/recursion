3a93f249c654880f443afa0640ff816c3ef5c0a8
Ben Childs <benjamin.t.childs@gmail.com>
2017-06-13 14:44:23 -0500

    Modified notes, begin framework for stringify (base cases)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,21 +5,48 @@
 
 var stringifyJSON = function(obj) {
   // input is any type of object or primitive or array, including functions and undefined
+  // per suggestion: think of single case, then of two, then of (n)
 
   //check type of input
-  	//if it is a function or undefined, ignore
+  var objType = typeof obj;
+
+  //if it is a function or undefined, ignore
+  if( obj === undefined || objType === "function" ){
+  	return;
+  }
+  	//base case (primitives)
+  	//string, number, boolean, obj ==== null, symbol, 
+    if( objType === "string"){
+
+    } else if( objType === "number"){
+
+    } else if( objType === "boolean"){
+
+    } else if( objType === "symbol"){
+
+    } else if( obj === null ){
+
+    }
+
+
   	//if it is any of the primitives
-  		//handle accordingly
+  	  //handle accordingly
+
+  	//recursive cases( array, obj )
   	//if it is an array
-  		//remember '[' and ']'
-  		//iterate across length of array
-  		//check type of input
-  	//otherwise, for each property
-  		//remember '{' and '}'
-  		//check the value type
+  	if( Array.isArray(obj) ) {
+  	  //remember '[' and ']'
+  	  //iterate across length of array
+  	  //check type of input
+  	} else {
+  	  //otherwise, for each property in this object
+  	  //remember '{' and '}'
+  	  //check the value type  		
+  	}
 
 
-  		//this will be done with a recursive function. Base cases are any of the primitives, since all objects/arrays eventually reduce down to a primitive
 
-  		//output is a string version of the input
+  	  //this will be done with a recursive function. Base cases are any of the primitives, since all objects/arrays eventually reduce down to a primitive
+
+  	  //output is a string version of the input
 };
