19af5a30069d2128b3dc736403e8d6499267440c
Ben Childs <benjamin.t.childs@gmail.com>
2017-06-14 11:36:14 -0500

    added initial getElementsByClassName notes, starting on base cases

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,40 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//You should use document.body, element.childNodes, and element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //input: className that we are searching for (string)
+  var output = [];
+  var body = document.body;
+
+  //will need to search all the child nodes of the body, and all child nodes of the child nodes
+  //need a function which checks for classname, number of childNodes, and then runs on each child node if any
+
+  //error - className is empty string, null, undefined, etc
+  if(className === undefined || className === "") { return; }
+
+  function check(elem, className) {
+	 	//base case - childNodes is empty
+	 	if ( elem.childNodes.length === 0 ) {
+	 		 //classList contains target
+	 		 if( elemen.classList.contains(className) ) { 
+	 		 	output.push(elem);
+	 		 	return; 
+	 		 } else {
+	 		 //classList does not contain target
+	 		  return;
+	 		}
+	 	}
+
+	  //recursive case - childNodes not empty, classList contains target
+	  //recursive case - childNodes not empty, classList does not contain target
+  }
+
+
+
+
+  //output: array of all nodes with class in input?
+  return output;
 };
