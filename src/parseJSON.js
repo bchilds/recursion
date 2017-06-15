c26fe084e6511849c2ff2ab533709d5cda15958a
Ben Childs <benjamin.t.childs@gmail.com>
2017-06-15 12:47:21 -0500

    initial outline for JSON parser

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,35 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  //input always a string
+  var index = 0;
+  var output = {};
+
+  function objOrArrayParse(json){
+	  //parseObj or Array
+		//first char is a { or [
+		//slice off outer object/array
+		//determine if next char an object or a value, call function for that
+
+	  if(json[index] === '{') {
+
+  	} else if(json[index] === '[') {
+
+  	}
+
+  }
+
+
+
+  //parseValue
+  	//check if string
+  		//if yes, find ending "
+  		//worry about escape characters
+  	//check if...
+  		//: next char, make this key and call value or object on next section (after the :)
+  		//, next char, call value or object on next section (after the ,)
+  //function valOrObjOrArray
+  	//takes in a string, returns whether the next item in array is a value, array or object, runs appropriate function
+
+  	return output;
 };
