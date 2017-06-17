3a2ad5792f553bb809c8460b100557cee4beb0f9
Ben Childs <benjamin.t.childs@gmail.com>
2017-06-17 16:33:56 -0500

    Passes all tests except final parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -7,17 +7,24 @@ var parseJSON = function(json) {
 
   function getElementIndices(json){
   	//returns an array of the element indices
-  	
+  	//also needs to check to make sure it is not within an array or object element inside checked obj/array
+
 		//get all element indices (this is the index of a comma)
 		//iterate across json to find each element
   	var elementIndices = [];
   	var end = 0;
   	var inQuotes = false;
+  	var objCounter = 0;
+  	var arrayCounter = 0;
 		for(end; end < json.length; end++){
 			//for each character in the obj string, check if it is a comma that is outside of a quote.
-			if(json[end] === '"') { 
+			if(json.charAt(end) === '"') { 
 				inQuotes = !inQuotes; 
-			} else if(json[end] === ',' && !inQuotes){
+			} else if(json.charAt(end)  === '{'){ objCounter++; }
+  		else if(json.charAt(end) === '['){ arrayCounter++; }
+  		else if(json.charAt(end) === ']'){ arrayCounter--; }
+  		else if(json.charAt(end) === '}'){ objCounter--; }
+  		else if(json.charAt(end) === ',' && !inQuotes && objCounter === 0 && arrayCounter === 0){
 				elementIndices.push(end);
 			}
 		}
@@ -29,20 +36,21 @@ var parseJSON = function(json) {
   function parseObj(json){
 		//find : index for each property, determineParse each property/key
 		var objOut = {};
-		var elementIndices = getElementIndices(json);
 
 		//now, for each element, find the : and determineParse each key/pair
 		//we know that we just want the elements and not the outer object brackets, so let's remove those now
-		elementJson = json.slice(1, json.length - 1);
+		var elementJson = json.slice(1, json.length - 1);
+		var elementIndices = getElementIndices(elementJson);
+
 		var start = 0;
 		for(var i = 0; i < elementIndices.length; i++){
 			if( findChar( ':', elementJson.slice(start, elementIndices[i] - 1) ) > 0 ){ //if it can find the colon
 				var colonIndex = findChar( ':', elementJson.slice(start, elementIndices[i] - 1) );
 				var spacer = 0;
 				if(elementJson[start + colonIndex + 1] === ' ') { spacer = 1;}
-				objOut[ determineParse( elementJson.slice(start, start + colonIndex) ) ] = determineParse( elementJson.slice(start + colonIndex + 1 + spacer, elementIndices[i] - 1) ); 
+				objOut[ determineParse( elementJson.slice(start, start + colonIndex) ) ] = determineParse( elementJson.slice(start + colonIndex + 1 + spacer, elementIndices[i]) ); 
 				//objOut[stringBeforeColon] = stringAfterColon
-				start = elementIndices[i] + 1;
+				start = elementIndices[i] + 1 + spacer;
 			}
 		}
 
@@ -53,7 +61,7 @@ var parseJSON = function(json) {
   	//input is a string, output is an array
   	//find each element and determineParse it
   	var array = [];
-  	elementJson = json.slice(1, json.length - 1);
+  	var elementJson = json.slice(1, json.length - 1);
   	var elementIndices = getElementIndices(elementJson);
   	var start = 0;
   	for(var i = 0; i < elementIndices.length; i++){
@@ -69,9 +77,36 @@ var parseJSON = function(json) {
   } //end parseArray
 
   function parseString(json){
-  	var outString = json.slice(1,json.length - 1);
+  	var outString = '';
+  	//var outString = json.slice(1,json.length - 1);
+  	var workingArray = json.slice(1,json.length - 1).split('');
+  	//need to go across string and deal with escape characters
+  	var escCounter = 0;
+  	for(var i = 0; i < workingArray.length; i++){
+  		if(workingArray[i] === "\\"){
+  			//workingArray.splice(i + 1,1);
+  			escCounter++;
+  			//if it's the last character, make sure to add it to throw syntax error
+  			if(i + 1 === workingArray.length && escCounter === 1){
+  				outString += workingArray[i];
+  			}
+  		} else {
+  			if(escCounter > 1 && escCounter % 2 === 1 && workingArray[i] === '"'){ //this line filters down multiple \
+  				outString += '\\';
+  			}
+  			outString += workingArray[i];
+  			escCounter = 0;
+  		}
+  		
+  	}
+
   	if(outString === undefined){ outString = ""; }
- 		return outString;
+
+  	if(outString.charAt(outString.length - 1) === '\\'){
+  		throw new SyntaxError('Invalid JSON');
+  	} else {
+ 			return outString;
+ 		}
   } //end parseString
 
   function parseValue(json){
@@ -98,12 +133,12 @@ var parseJSON = function(json) {
   	var indexOut = -1;
 
   	for(var i = 1; i < string.length; i++){
-  		if(string[i] === '{'){ objCounter++; }
-  		else if(string[i] === '['){ arrayCounter++; }
-  		else if(string[i] === ']'){ arrayCounter--; }
-  		else if(string[i] === '}'){ objCounter--; }
+  		if(string.charAt(i) === '{'){ objCounter++; }
+  		else if(string.charAt(i) === '['){ arrayCounter++; }
+  		else if(string.charAt(i) === ']'){ arrayCounter--; }
+  		else if(string.charAt(i) === '}'){ objCounter--; }
 
-  		if(string[i] === char && objCounter === 0 && arrayCounter === 0 ){
+  		if(string.charAt(i) === char && objCounter === 0 && arrayCounter === 0 ){
   			indexOut = i;
   		}
   	}
@@ -115,38 +150,34 @@ var parseJSON = function(json) {
   function determineParse(json){
   	//run on json to run appropriate functions on each element as we come across it
   	
-		var initChar = json[0];
+		var initChar = json.charAt(0);
 
 		if(initChar === '{'){
-			if(json[json.length - 1] === '}'){
+			if(json.charAt(json.length - 1) === '}'){
 				return parseObj(json);
 			} else { 
-				//throw error
+				throw new SyntaxError('Incomplete object, lacking brace');
 			}
 		} else if(initChar === '[') {
-			if(json[json.length - 1] === ']'){
+			if(json.charAt(json.length - 1) === ']'){
 				return parseArray(json);
 			} else { 
-				//throw error
+				throw new SyntaxError('Incomplete array, lacking bracket');
 			}
 
 		} else if(initChar === '"') {
-			return parseString(json);
+			if(json.charAt(json.length - 1) === '"'){
+				return parseString(json);
+			} else { 
+				throw new SyntaxError('Incomplete string, lacking closing "');
+			}
 		} else if(initChar === '\\'){
 			//do something
 		} else {
 			//it's a value, run parseValue
 			return parseValue(json.trim());
 		}
-  	//get first char of element
-  	//if it is {, we will parse obj
-  		//iterate along to find matching }, will need counter
-  	//if it is [, we will parse array
-  		//iterate along to find matching ], will need counter
-  	//if it is ", we will parse string
-  		//iterate until find next ", everything in that is the string
-  	//otherwise, parse as value
-  	//handle \
+  	
   }
 
   	return determineParse(json);
